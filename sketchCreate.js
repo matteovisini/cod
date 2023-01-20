900f9c3110dd17860bd118453bb6ac09f3c2f363
matteovisini <matteo.visini.99@gmail.com>
2023-01-20 14:16:21 +0100

    Update sketchCreate.js

--- sketchCreate.js
+++ sketchCreate.js
@@ -121,8 +121,8 @@ function setup() {
 let canva =createCanvas(windowWidth, windowHeight);
   canva.parent("canvasp5");
   colorMode(HSB);
-  lat = 45.43248464489723//locationData.latitude + fakeLat//Cambia questo per forzare la tua lat
-  lng = 8.593096490762926//locationData.longitude+ fakeLng//Cambia questo per forzare la tua lng
+  lat = 45.39452652373571//locationData.latitude + fakeLat//Cambia questo per forzare la tua lat
+  lng = 8.925698074890695//locationData.longitude+ fakeLng//Cambia questo per forzare la tua lng
   acc = locationData.accuracy
   console.log("Your current position is:", lat, lng, "accuracy:", acc)
   
@@ -188,8 +188,8 @@ let starType
 function checkStelle() {
   //set laptop location
  
-  laptopLat = 45.43248464489723 //lat //Cambia questo per forzare la tua lat
-  laptopLng = 8.593096490762926 //lng //Cambia questo per forzare la tua lng
+  laptopLat = 45.39452652373571 //lat //Cambia questo per forzare la tua lat
+  laptopLng = 8.925698074890695 //lng //Cambia questo per forzare la tua lng
   laptopAcc = acc
 
   // variabili per controllare il ciclo
